32c2580b46c26b7e32c30359eb4f4f14990a5d7a
gnawhleinad <danielleehwang@gmail.com>
2014-07-22 22:06:04 -0700

    remove accidental checkin

--- trask/__main__.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-if __package__ is None and not hasattr(sys, 'frozen'):
-	import os.path
-	path = os.path.realpath(os.path.abspath(__file))
-	sys.path.append(os.path.dirname(os.path.dirname(path)))
